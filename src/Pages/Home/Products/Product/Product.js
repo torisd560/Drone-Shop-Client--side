797fdbfab559de4bcfe603d3183944a3f6bcf2df
torisd560 <mitalisd123@gmail.com>
2021-11-27 14:17:53 +0600

    added all updates

--- src/Pages/Home/Products/Product/Product.js
+++ src/Pages/Home/Products/Product/Product.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Card, Col, Button } from 'react-bootstrap';
+import { Col, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Product = (props) => {
     const { img, name, price, _id } = props.product
     return (
         <Col>
-            <Card className='p-5 shadow-sm border-0 rounded-1' >
-                <Card.Img variant="top" src={img} className='img-fluid' style={{ width: '500px', height: '250px', }} />
-                <Card.Body>
-                    <Card.Title className=' fw-bold'>{name}</Card.Title>
-                    <p className='custom-text-pink fs-5'>$ {price}.00</p>
+            <div className='p-4  border-0 rounded-1 bg-light-gray' >
+                <img src={img} className='img-fluid' alt="" />
+                <div  className=' mt-3'>
+                    <h5>{name}</h5>
+                    <p className='custom-text-pink fw-bold fs-5'>$ {price}.00</p>
                     <Link to={`/moreProducts/placeOrder/${_id}`}>
                         <Button className='custom-btn'><i className="fas fa-shopping-cart me-2"></i>ORDER NOW</Button>
                     </Link>
-                </Card.Body>
-            </Card>
+                </div>
+            </div>
         </Col>
 
 
