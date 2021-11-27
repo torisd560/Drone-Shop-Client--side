9e9017ce766ebbac9c3155e5a30389f03e6f130e
torisd560 <mitalisd123@gmail.com>
2021-11-27 22:41:11 +0600

    added top banner

--- src/Pages/Home/TopBanner/TopBanner.js
+++ src/Pages/Home/TopBanner/TopBanner.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Carousel , Button} from 'react-bootstrap'
+import { Carousel, Button } from 'react-bootstrap'
 import slider1 from '../../../images/slider1.jpg'
 import slider2 from '../../../images/slider2.jpg'
 import slider3 from '../../../images/slide3.jpg'
@@ -12,12 +12,12 @@ const TopBanner = () => {
                     src={slider1}
                     alt="First slide"
                 />
-                <Carousel.Caption  className = 'text-start' style = {{top : '40%'}}>
-                <h4 className = 'fw-bold custom-text-pink' style = {{fontSize : '25px' }}>Welcome To Dronet</h4>
-                    <h1 className = 'fw-bold' style = {{fontSize : '40px'}}>EXPLORE THE WORLD
+                <Carousel.Caption className='text-start' style={{ top: '40%' }}>
+                    <h4 className='fw-bold custom-text-pink' style={{ fontSize: '25px' }}>Welcome To Dronet</h4>
+                    <h1 className='fw-bold' style={{ fontSize: '40px' }}>EXPLORE THE WORLD
                         <br /> WITH NEW DRONE
                         <br /> TECHNOLOGY</h1>
-                        <Button className = ' custom-btn'>EXPLORE MORE</Button>
+                    <Button className=' custom-btn'>EXPLORE MORE</Button>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item >
@@ -27,11 +27,12 @@ const TopBanner = () => {
                     alt="Second slide"
                 />
 
-                <Carousel.Caption  className = 'text-start' style = {{top : '40%'}}>
-                <h4 className = 'fw-bold custom-text-pink' style = {{fontSize : '25px' }}>Welcome To Dronet</h4>
-                    <h1 className = 'fw-bold' style = {{fontSize : '40px'}}>EXPLORE THE WORLD
+                <Carousel.Caption className='text-start' style={{ top: '40%' }}>
+                    <h4 className='fw-bold custom-text-pink' style={{ fontSize: '25px' }}>Welcome To Dronet</h4>
+                    <h1 className='fw-bold' style={{ fontSize: '40px' }}>EXPLORE THE WORLD
                         <br /> MOST POPULAR DRONE
                         <br /> TECHNOLOGY</h1>
+                    <Button className=' custom-btn'>EXPLORE MORE</Button>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -41,11 +42,12 @@ const TopBanner = () => {
                     alt="Third slide"
                 />
 
-                <Carousel.Caption  className = 'text-start'  style = {{top : '40%'}}>
-                    <h4 className = 'fw-bold custom-text-pink' style = {{fontSize : '25px' }}>Welcome To Dronet</h4>
-                    <h1 className = 'fw-bold' style = {{fontSize : '40px'}}>OUR PROFFESSIONAL
+                <Carousel.Caption className='text-start' style={{ top: '40%' }}>
+                    <h4 className='fw-bold custom-text-pink' style={{ fontSize: '25px' }}>Welcome To Dronet</h4>
+                    <h1 className='fw-bold' style={{ fontSize: '40px' }}>OUR PROFFESSIONAL
                         <br /> DRONE SERVICE
                         <br /> TECHNOLOGY</h1>
+                    <Button className=' custom-btn'>EXPLORE MORE</Button>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
